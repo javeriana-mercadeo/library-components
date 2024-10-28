f36e8d45c4d1fa196ea119484fd11df3f6befba1
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-10-28 10:34:42 -0500

    add: se ajusta el pop up de alertas para ser modificado desde liferay

--- pop_up/popup_alert/app.js
+++ pop_up/popup_alert/app.js
@@ -1,41 +1,13 @@
-const elements = { buttonModalAlert: null, modal: null };
-
-window.addEventListener("DOMContentLoaded", () => {
+$(document).ready(() => {
     console.log("🟠 [Modal de alerta] Cargando...");
-    runMain(elements);
+    $("#modalAlert").modal("show");
     console.log("🟢 [Modal de alerta] Cargado con éxito");
+
+    $("#buttonModalAlert").on("click", function () {
+        $("#modalAlert").modal("hide");
+    });
+
+    const editMode = $(".has-edit-mode-menu");
+
+    if (editMode) $("#modalAlert").addClass("show");
 });
-
-// FUNCIONES
-function runMain(elements) {
-    loadElements(elements);
-    const { buttonModalAlert, modal } = elements;
-
-    // VALIDAR SI LOS ELEMENTOS EXISTEN
-    if (validateElements(elements)) {
-        buttonModalAlert.style.display = "flex";
-        modal.style.display = "block";
-        renderModalAlert(elements);
-    } else {
-        buttonModalAlert.style.display = "none";
-        modal.style.display = "none";
-    }
-}
-
-// Cargar los elementos
-function loadElements(elements) {
-    elements.buttonModalAlert = document.getElementById("buttonModalAlert");
-    elements.modal = document.getElementById("modalAlert");
-}
-
-// Validar si los elementos existen
-function validateElements(elements) {
-    return Object.keys(elements).every((key) => elements[key] !== null);
-}
-
-function renderModalAlert(elements) {
-    const { buttonModalAlert } = elements;
-
-    // MANEJAR LOS EVENTOS
-    buttonModalAlert.click();
-}
