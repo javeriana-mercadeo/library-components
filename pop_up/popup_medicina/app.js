d6de740a562577101a1dee7676bc2750d38aca24
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-11-20 23:57:55 -0500

    avance en pop-up de medicina

---
+++ pop_up/popup_medicina/app.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+  console.log("🟠 [Modal de alerta] Cargando...");
+  $("#modalAlert").modal("show");
+  console.log("🟢 [Modal de alerta] Cargado con éxito");
+
+  $("#buttonModalAlert").on("click", function () {
+    $("#modalAlert").modal("hide");
+  });
+
+  const editMode = $(".has-edit-mode-menu");
+
+  if (editMode) $("#modalAlert").addClass("show");
+});
