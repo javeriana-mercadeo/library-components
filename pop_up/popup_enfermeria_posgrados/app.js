27b24ff1c7d1484c830da63093ffe46b00499b41
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2024-12-06 17:43:20 -0500

    fix: se ajusta el pop-up azul con graduente

--- pop_up/popup_enfermeria_posgrados/app.js
+++ pop_up/popup_enfermeria_posgrados/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-    $('#promoModal').modal('show');
+$(document).ready(() => {
+    console.log("🟠 [Modal de alerta] Cargando...");
+    $("#promoModal").modal("show");
+    console.log("🟢 [Modal de alerta] Cargado con éxito");
+
+    $("#promoModal-btn-close").on("click", function () {
+        $("#promoModal").modal("hide");
+    });
+
+    const editMode = $(".has-edit-mode-menu");
+
+    if (editMode) $("#promoModal").addClass("show");
 });
