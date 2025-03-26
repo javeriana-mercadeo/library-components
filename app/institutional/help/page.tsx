c19441e4ec271046b98a609cc4c3dac30a07b0cd
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2025-03-26 09:42:26 -0500

    help created

---
+++ app/institutional/help/page.tsx
@@ -0,0 +1,12 @@
+import HelpHero from '@library/_institutional/sections/helpHero'
+
+const Page = () => {
+  return (
+    <div>
+      Nueva página
+      <HelpHero />
+    </div>
+  )
+}
+
+export default Page
