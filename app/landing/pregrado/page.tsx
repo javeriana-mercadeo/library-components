6a1b39259a22a62edaa45e922d2fd13d7abb1cc3
krlaNicol <karla.gonzalez@genstudents.org>
2025-05-28 16:30:31 -0500

    organización de variables y cambio de nombre de carpeta

--- app/landing/pregrado/page.tsx
+++ app/landing/pregrado/page.tsx
@@ -21,6 +21,8 @@ export default function Profesional() {
         <DatosProgramaVideo />
       </ViewComponent> */}
 
+      
+
       <ViewComponent path={`${basePath}/sections/meetingDirector`}>
         <MeetingDirector />
       </ViewComponent>
