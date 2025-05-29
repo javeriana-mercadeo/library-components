7d671b3325ddbf5c9731efaa21c286d50e50e034
Jose Moreno <jgmorenor277@gmail.com>
2025-05-29 05:40:02 -0500

    Inicializado el index.jsx e info.json de sección materias por semestre

---
+++ app/landing/pregrado/sections/materiasPorSemestre/index.jsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect } from 'react'
+import Btn from '@library/components/buttons/btn_general'
+import Container from '@library/components/container/Container'
+
+import logo from '../../../../../assets/logos/logo-jave-h-blue.svg'
+import logoDark from '../../../../../assets/logos/logo-jave-h-white.svg'
+import script from './script.js'
+
+import './styles.scss'
+
+const MateriasSemestre = () => {
+    // Ejecutar el script cuando el componente se monta
+        useEffect(() => {
+        script()
+    }, [])
+
+    return (
+        <>
+        </>
+    )
+}
