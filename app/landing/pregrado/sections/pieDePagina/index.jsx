5040f0839914e24986c57074da9aea786ddebba3
krlaNicol <karla.gonzalez@genstudents.org>
2025-06-01 22:39:08 -0500

    corrección de variables style footer

--- app/landing/pregrado/sections/pieDePagina/index.jsx
+++ app/landing/pregrado/sections/pieDePagina/index.jsx
@@ -6,90 +6,87 @@ import LogoBlanco from '@library/components/logo_institucional_blanco'
 import UniversidadTour from '../../../../../assets/pieDePagina/universidad-tour.gif'
 import Vigilada from '../../../../../assets/pieDePagina/vigilada.png'
 
-
 import './styles.scss'
 
 const PieDePagina = () => {
-  useEffect(() => {
-
-  }, [])
+  useEffect(() => {}, [])
 
   return (
     <section id="footer">
       <div className="color-container-footer">
         <Container>
-        <footer className="container footer">
-          <div className="footer__container">
-            <div className="footer__left">
-              <LogoBlanco  alt="LogoBlanco " className="footer__logo light" />
-              <h3 className="footer__title">
-                Explora nuestro campus <br /> y su tecnología de punta.
-              </h3>
-            </div>
-            <div className="footer__middle">
-              <div className="footer__video">
-                <div className="footer__mp4">
-                  <img src={UniversidadTour.src} alt="Descripción del GIF" className="video-gif" />
-                  <a
-                    href="https://www.javeriana.edu.co/tour-virtual-javeriana/index.htm"
-                    target="_blank"
-                    className="video-overlay"
-                    rel="noopener noreferrer"></a>
+          <footer className="container footer">
+            <div className="footer__container">
+              <div className="footer__left">
+                <LogoBlanco alt="LogoBlanco " className="footer__logo light" />
+                <h3 className="footer__title">
+                  Explora nuestro campus <br /> y su tecnología de punta.
+                </h3>
+              </div>
+              <div className="footer__middle">
+                <div className="footer__video">
+                  <div className="footer__mp4">
+                    <img src={UniversidadTour.src} alt="Descripción del GIF" className="video-gif" />
+                    <a
+                      href="https://www.javeriana.edu.co/tour-virtual-javeriana/index.htm"
+                      target="_blank"
+                      className="video-overlay"
+                      rel="noopener noreferrer"></a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="footer__right">
-              <div className="footer__contact">
-                <p className="footer__container_contact">
-                  <span className="footer__contact_title">Dirección:</span> Cra. 7 #40-62, Chapinero, Bogotá, Colombia.
-                </p>
-                <p className="footer__container_contact">
-                  <span className="footer__contact_title">Teléfono:</span> +57 (601) 320 8320
-                </p>
-                <p className="footer__container_contact">
-                  <span className="footer__contact_title">Email:</span>{' '}
-                  <a href="mailto:atencionaspirantes@javeriana.edu.co">atencionaspirantes@javeriana.edu.co</a>
-                </p>
-              </div>
-              <div className="footer__social">
-                <div className="footer__icons">
-                  <p>Síguenos en nuestras redes sociales:</p>
-                  <div className="footer__icons-links">
-                    <a href="#">
-                      <i className="ph ph-facebook-logo"></i>
-                    </a>
-                    <a href="#">
-                      <i className="ph ph-instagram-logo"></i>
-                    </a>
-                    <a href="#">
-                      <i className="ph ph-linkedin-logo"></i>
-                    </a>
-                    <a href="#">
-                      <i className="ph ph-tiktok-logo"></i>
-                    </a>
+              <div className="footer__right">
+                <div className="footer__contact">
+                  <p className="footer__container_contact">
+                    <span className="footer__contact_title">Dirección:</span> Cra. 7 #40-62, Chapinero, Bogotá, Colombia.
+                  </p>
+                  <p className="footer__container_contact">
+                    <span className="footer__contact_title">Teléfono:</span> +57 (601) 320 8320
+                  </p>
+                  <p className="footer__container_contact">
+                    <span className="footer__contact_title">Email:</span>{' '}
+                    <a href="mailto:atencionaspirantes@javeriana.edu.co">atencionaspirantes@javeriana.edu.co</a>
+                  </p>
+                </div>
+                <div className="footer__social">
+                  <div className="footer__icons">
+                    <p>Síguenos en nuestras redes sociales:</p>
+                    <div className="footer__icons-links">
+                      <a href="#">
+                        <i className="ph ph-facebook-logo"></i>
+                      </a>
+                      <a href="#">
+                        <i className="ph ph-instagram-logo"></i>
+                      </a>
+                      <a href="#">
+                        <i className="ph ph-linkedin-logo"></i>
+                      </a>
+                      <a href="#">
+                        <i className="ph ph-tiktok-logo"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="footer__divider"></div>
+            <div className="footer__divider"></div>
 
-          <div className="footer__bottom">
-            <div className="footer__vigilada">
-              <img src={Vigilada.src} alt="Logo" className="footer__logoV" />
-              <p className="footer__info">
-                Sujeta a inspección y vigilancia por parte del Ministerio de <br />
-                Educación Nacional (Artículo 39 del decreto 1295 de 2010).
-              </p>
+            <div className="footer__bottom">
+              <div className="footer__vigilada">
+                <img src={Vigilada.src} alt="Logo" className="footer__logoV" />
+                <p className="footer__info">
+                  Sujeta a inspección y vigilancia por parte del Ministerio de <br />
+                  Educación Nacional (Artículo 39 del decreto 1295 de 2010).
+                </p>
+              </div>
+              <p>Copyright © 2024 Pontificia Universidad Javeriana</p>
+              <div className="footer__links">
+                <a href="#">Políticas de Privacidad</a>
+                <a href="#">Accesibilidad Digital</a>
+              </div>
             </div>
-            <p>Copyright © 2024 Pontificia Universidad Javeriana</p>
-            <div className="footer__links">
-              <a href="#">Políticas de Privacidad</a>
-              <a href="#">Accesibilidad Digital</a>
-            </div>
-          </div>
-        </footer>
+          </footer>
         </Container>
       </div>
     </section>
