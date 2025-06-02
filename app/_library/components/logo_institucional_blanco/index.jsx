6ebae0f032076f212be3b3192504d9d9d662018d
krlaNicol <karla.gonzalez@genstudents.org>
2025-06-02 17:46:17 -0500

    adaptación de temas oscuros

--- app/_library/components/logo_institucional_blanco/index.jsx
+++ app/_library/components/logo_institucional_blanco/index.jsx
@@ -4,6 +4,7 @@ const LogoBlanco = ({ className }) => {
   return (
     <figure className={'logo' + (className ? ` ${className}` : '')}>
       <img src="https://www.javeriana.edu.co/recursosdb/d/info-prg/logo-jave-h-white" alt="LogoBlanco" className="logo-image light" />
+            <img src="https://www.javeriana.edu.co/recursosdb/d/info-prg/logo-jave-h-white" alt="LogoBlanco" className="logo-image dark" />
     </figure>
   )
 }
