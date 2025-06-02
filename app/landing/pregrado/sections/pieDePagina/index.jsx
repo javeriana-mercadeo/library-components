0f329a95f2ef2b50b515c5756222c1186953aaa8
krlaNicol <karla.gonzalez@genstudents.org>
2025-06-02 15:12:00 -0500

    container

--- app/landing/pregrado/sections/pieDePagina/index.jsx
+++ app/landing/pregrado/sections/pieDePagina/index.jsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
 import Container from '@library/components/container/Container'
 import LogoBlanco from '@library/components/logo_institucional_blanco'
 import UniversidadTour from '../../../../../assets/pieDePagina/universidad-tour.gif'
@@ -9,16 +8,14 @@ import Vigilada from '../../../../../assets/pieDePagina/vigilada.png'
 import './styles.scss'
 
 const PieDePagina = () => {
-  useEffect(() => {}, [])
-
   return (
-    <section id="footer">
+    <footer id="footer">
       <div className="color-container-footer">
-        <Container>
-          <footer className="container footer">
+        <div className="footer">
+          <Container>
             <div className="footer__container">
               <div className="footer__left">
-                <LogoBlanco alt="LogoBlanco " className="footer__logo light" />
+                <LogoBlanco alt="Logo Pontificia Universidad Javeriana" className="footer__logo light" />
                 <h3 className="footer__title">
                   Explora nuestro campus <br /> y su tecnología de punta.
                 </h3>
@@ -26,12 +23,13 @@ const PieDePagina = () => {
               <div className="footer__middle">
                 <div className="footer__video">
                   <div className="footer__mp4">
-                    <img src={UniversidadTour.src} alt="Descripción del GIF" className="video-gif" />
+                    <img src={UniversidadTour.src} alt="Tour virtual universidad" className="video-gif" />
                     <a
                       href="https://www.javeriana.edu.co/tour-virtual-javeriana/index.htm"
                       target="_blank"
                       className="video-overlay"
-                      rel="noopener noreferrer"></a>
+                      rel="noopener noreferrer"
+                      aria-label="Ir al tour virtual de la universidad"></a>
                   </div>
                 </div>
               </div>
@@ -44,7 +42,7 @@ const PieDePagina = () => {
                     <span className="footer__contact_title">Teléfono:</span> +57 (601) 320 8320
                   </p>
                   <p className="footer__container_contact">
-                    <span className="footer__contact_title">Email:</span>{' '}
+                    <span className="footer__contact_title">Email:</span>
                     <a href="mailto:atencionaspirantes@javeriana.edu.co">atencionaspirantes@javeriana.edu.co</a>
                   </p>
                 </div>
@@ -52,16 +50,16 @@ const PieDePagina = () => {
                   <div className="footer__icons">
                     <p>Síguenos en nuestras redes sociales:</p>
                     <div className="footer__icons-links">
-                      <a href="#">
+                      <a href="#" aria-label="Facebook">
                         <i className="ph ph-facebook-logo"></i>
                       </a>
-                      <a href="#">
+                      <a href="#" aria-label="Instagram">
                         <i className="ph ph-instagram-logo"></i>
                       </a>
-                      <a href="#">
+                      <a href="#" aria-label="LinkedIn">
                         <i className="ph ph-linkedin-logo"></i>
                       </a>
-                      <a href="#">
+                      <a href="#" aria-label="TikTok">
                         <i className="ph ph-tiktok-logo"></i>
                       </a>
                     </div>
@@ -74,10 +72,9 @@ const PieDePagina = () => {
 
             <div className="footer__bottom">
               <div className="footer__vigilada">
-                <img src={Vigilada.src} alt="Logo" className="footer__logoV" />
+                <img src={Vigilada.src} alt="Logo vigilada por el Ministerio de Educación" className="footer__logoV" />
                 <p className="footer__info">
-                  Sujeta a inspección y vigilancia por parte del Ministerio de <br />
-                  Educación Nacional (Artículo 39 del decreto 1295 de 2010).
+                  Sujeta a inspección y vigilancia por parte del Ministerio de Educación Nacional (Artículo 39 del decreto 1295 de 2010).
                 </p>
               </div>
               <p>Copyright © 2024 Pontificia Universidad Javeriana</p>
@@ -86,10 +83,10 @@ const PieDePagina = () => {
                 <a href="#">Accesibilidad Digital</a>
               </div>
             </div>
-          </footer>
-        </Container>
+          </Container>
+        </div>
       </div>
-    </section>
+    </footer>
   )
 }
 
