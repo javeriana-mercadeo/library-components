32fe417af0314c2745dc3ce7854c480ff8c048ca
krlaNicol <karla.gonzalez@genstudents.org>
2025-06-02 20:39:13 -0500

    pendientes componentes

--- app/landing/pregrado/sections/perfilesPrograma/index.jsx
+++ app/landing/pregrado/sections/perfilesPrograma/index.jsx
@@ -3,7 +3,6 @@
 import { useEffect } from 'react'
 import Container from '@library/components/container/Container'
 import Paragraph from '@library/components/contain/paragraph'
-
 import perfilIngresoImg from '../../../../../assets/perfiles/perfil-ingreso.png'
 import perfilEgresoImg from '../../../../../assets/perfiles/perfil-egreso.png'
 import script from './script.js'
@@ -23,7 +22,6 @@ const PerfilesPrograma = () => {
             <h2 className="program-profile__title">Este programa es para ti si...</h2>
 
             <div className="program-profile__accordion">
-              {/* Botones del acordeón*/}
               <div className="program-profile__buttons">
                 <button type="button" className="program-profile__accordion-button active" data-content="content-1">
                   <span>Perfil de Ingreso</span>
@@ -37,13 +35,12 @@ const PerfilesPrograma = () => {
                   <span>Diferenciales</span>
                 </button>
               </div>
-              {/*Contenido del acordeón (inicialmente oculto)*/}
               <div id="content-1" className="program-profile__content-item">
                 <div className="program-profile__image_content">
                   <img src={perfilIngresoImg.src} alt="Perfil de Ingreso" className="program-profile__image" />
                 </div>
                 <div className="p-content">
-                  <Paragraph isEditable={true}>
+                  <Paragraph>
                     <p>
                       Esperamos que los aspirantes al programa de Ingeniería Civil de la Universidad Javeriana tengan interés en diseñar,
                       construir y gestionar proyectos de infraestructura que transformen la sociedad de manera sostenible.
@@ -58,27 +55,35 @@ const PerfilesPrograma = () => {
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Te apasiona la solución de problemas en el diseño, construcción y operación de infraestructura civil.
-                      </p>
+                      <Paragraph>
+                        <p className="program-profile__text">
+                          Te apasiona la solución de problemas en el diseño, construcción y operación de infraestructura civil.
+                        </p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">Tienes habilidades en matemáticas, análisis y resolución de problemas.</p>
+                      <Paragraph>
+                        <p className="program-profile__text">Tienes habilidades en matemáticas, análisis y resolución de problemas.</p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">Te interesa la sostenibilidad y el impacto ambiental de las construcciones.</p>
+                      <Paragraph>
+                        <p className="program-profile__text">Te interesa la sostenibilidad y el impacto ambiental de las construcciones.</p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">Disfrutas el trabajo en equipo en proyectos multidisciplinarios.</p>
+                      <Paragraph>
+                        <p className="program-profile__text">Disfrutas el trabajo en equipo en proyectos multidisciplinarios.</p>
+                      </Paragraph>
                     </div>
                   </div>
                 </div>
@@ -88,7 +93,7 @@ const PerfilesPrograma = () => {
                   <img src={perfilEgresoImg.src} alt="Perfil de Egreso" className="program-profile__image" />
                 </div>
                 <div className="p-content">
-                  <Paragraph isEditable={true}>
+                  <Paragraph>
                     <p>
                       Los egresados de Ingeniería Civil de la Universidad Javeriana son profesionales capacitados para diseñar, construir y
                       gestionar proyectos de infraestructura con un enfoque sostenible y ético.
@@ -103,34 +108,42 @@ const PerfilesPrograma = () => {
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Aplican conocimientos técnicos para resolver problemas en todas las etapas del ciclo de vida de proyectos de
-                        infraestructura civil.
-                      </p>
+                      <Paragraph className="program-profile__text">
+                        <p >
+                          Aplican conocimientos técnicos para resolver problemas en todas las etapas del ciclo de vida de proyectos de
+                          infraestructura civil.
+                        </p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Integran factores sociales, económicos y ambientales en la toma de decisiones.
-                      </p>
+                      <Paragraph>
+                        <p className="program-profile__text">
+                          Integran factores sociales, económicos y ambientales en la toma de decisiones.
+                        </p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Ejercen su profesión con responsabilidad ética y compromiso con el desarrollo sostenible.
-                      </p>
+                      <Paragraph>
+                        <p className="program-profile__text">
+                          Ejercen su profesión con responsabilidad ética y compromiso con el desarrollo sostenible.
+                        </p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Demuestran liderazgo y trabajan en equipos interdisciplinarios de manera colaborativa y efectiva.
-                      </p>
+                      <Paragraph>
+                        <p className="program-profile__text">
+                          Demuestran liderazgo y trabajan en equipos interdisciplinarios de manera colaborativa y efectiva.
+                        </p>
+                      </Paragraph>
                     </div>
                   </div>
                 </div>
@@ -141,7 +154,7 @@ const PerfilesPrograma = () => {
                 </div>
 
                 <div className="p-content">
-                  <Paragraph isEditable={true}>
+                  <Paragraph>
                     <p>
                       La carrera de Ingeniería Civil en la Universidad Javeriana se distingue por su enfoque innovador y compromiso con la
                       sostenibilidad.
@@ -156,21 +169,27 @@ const PerfilesPrograma = () => {
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">
-                        Uso de tecnología avanzada y herramientas digitales para la planificación y ejecución de proyectos.
-                      </p>
+                      <Paragraph>
+                        <p className="program-profile__text">
+                          Uso de tecnología avanzada y herramientas digitales para la planificación y ejecución de proyectos.
+                        </p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">Formación interdisciplinaria con enfoque en soluciones innovadoras.</p>
+                      <Paragraph>
+                        <p className="program-profile__text">Formación interdisciplinaria con enfoque en soluciones innovadoras.</p>
+                      </Paragraph>
                     </div>
                     <div className="program-profile__item">
                       <div className="program-profile__icon-container">
                         <i className="ph ph-check-circle"></i>
                       </div>
-                      <p className="program-profile__text">Compromiso con la sostenibilidad y el desarrollo social.</p>
+                      <Paragraph>
+                        <p className="program-profile__text">Compromiso con la sostenibilidad y el desarrollo social.</p>
+                      </Paragraph>
                     </div>
                   </div>
                 </div>
