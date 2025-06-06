911d806fc37032d377ed976979e5821e3922030b
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2025-06-06 09:03:21 -0500

    add: se avanza con la implementación del componente de experiencia

--- app/landing/pregrado/sections/7_experiencia/index.jsx
+++ app/landing/pregrado/sections/7_experiencia/index.jsx
@@ -1,51 +1,43 @@
 'use client'
 
 import { useEffect } from 'react'
-import Title from '@/app/_library/components/contain/title'
+import Container from '@library/components/container'
+import Title from '@library/components/contain/title'
 import Paragraph from '@library/components/contain/paragraph'
-import Container from '@/app/_library/components/container'
-
-import linkedInImg from './assets/linkedin.svg'
 
 import script from './script.js'
 import info from './info.json'
 import './styles.scss'
 
 const Experiencia = () => {
-  // Base para IDs únicos
-  const base = 'experiencia-javeriana'
-  const elementName = info.id || 'experience-carousel'
+  const elementName = info.id || 'experiencia'
   const baseClass = 'experience-carousel'
 
   useEffect(() => {
     script()
   }, [])
 
-  {
-    /* <h2 className="experience-carousel__title">Vive la Experiencia Javeriana</h2> */
-  }
-
-  // Datos del carrusel
+  // Datos del carrusel - estos vendrán de Liferay
   const carouselData = [
     {
       type: 'image',
-      src: 'https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg',
+      src: 'https://www.javeriana.edu.co/recursosdb/d/info-prg/dsc00979',
       alt: 'Estudiantes Javeriana',
       link: '#'
     },
     {
       type: 'testimonial',
-      text: 'Actualmente me desempeño en la organización Terpel en el área de Inteligencia de Negocios. Mis funciones en la compañía son hacer estudios de mercado y encontrar oportunidades de mejora, que permitan al área comercial, de mercadeo y financiera potenciar los resultados de la compañía.',
+      text: 'Actualmente me desempeño en la organización Terpel en el área de Inteligencia de Negocios. Mis funciones en la compañía son hacer estudios de mercado y encontrar oportunidades de mejora.',
       user: {
         name: 'Carlos Gómez',
-        job: 'Urbanista en CityScape',
-        avatar: 'https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250',
+        job: 'Analista de Negocios en Terpel',
+        avatar: 'https://www.javeriana.edu.co/recursosdb/d/info-prg/avatar-1',
         linkedin: '#'
       }
     },
     {
       type: 'video',
-      src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+      videoId: 'IWZvfiu3gX4',
       link: '#'
     },
     {
@@ -53,54 +45,55 @@ const Experiencia = () => {
       text: 'La Javeriana me dio las herramientas para liderar proyectos empresariales. Hoy impulso estrategias de crecimiento en una multinacional.',
       user: {
         name: 'Anamaría López',
-        job: 'Gerente de estrategia en GlobalCorp',
-        avatar: 'https://gravatar.com/images/homepage/avatar-07.png',
-        linkedin: '#'
-      }
-    },
-    {
-      type: 'video',
-      src: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-      link: '#'
-    },
-    {
-      type: 'testimonial',
-      text: 'Mi paso por la Javeriana fue clave para llegar a donde estoy. Ahora trabajo en diseño urbano, creando ciudades más inclusivas.',
-      user: {
-        name: 'Isabella Martínez',
-        job: 'Economista',
-        avatar: 'https://pfpmaker.com/images/ai/examples/first/results/result-pic-1.png',
+        job: 'Gerente de Estrategia en GlobalCorp',
+        avatar: 'https://www.javeriana.edu.co/recursosdb/d/info-prg/avatar-2',
         linkedin: '#'
       }
     }
   ]
 
   // Componente para renderizar imagen
-  const ImageCard = ({ src, alt, link }) => (
-    <a href={link} className="card-link-7">
-      <div className="image-card-7">
-        <img src={src} alt={alt} className="experience-carousel__image" />
+  const ImageCard = ({ src, alt, link, index }) => (
+    <a
+      href={link}
+      className={`${baseClass}__card-link`}
+      data-lfr-editable-id={`experience-image-link-${index}`}
+      data-lfr-editable-type="link">
+      <div className={`${baseClass}__image-card`}>
+        <img
+          src={src}
+          alt={alt}
+          className={`${baseClass}__image`}
+          data-lfr-editable-id={`experience-image-${index}`}
+          data-lfr-editable-type="image"
+        />
       </div>
     </a>
   )
 
   // Componente para renderizar testimonial
   const TestimonialCard = ({ text, user, index }) => (
-    <div className="testimonial-card">
-      <Paragraph className="testimonial-text" id={`${elementName}-testimonial-text-${index}`}>
+    <div className={`${baseClass}__testimonial-card`}>
+      <Paragraph className={`${baseClass}__testimonial-text`} id={`${elementName}-testimonial-text-${index}`} isEditable={false}>
         {text}
       </Paragraph>
-      <div className="testimonial-user">
-        <div className="testimonial-avatar">
-          <img src={user.avatar} alt={user.name} />
+      <div className={`${baseClass}__testimonial-user`}>
+        <div className={`${baseClass}__testimonial-avatar`}>
+          <img src={user.avatar} alt={user.name} data-lfr-editable-id={`experience-avatar-${index}`} data-lfr-editable-type="image" />
         </div>
-        <div className="testimonial-info">
-          <h4 className="testimonial-name">{user.name}</h4>
-          <Paragraph className="testimonial-job" id={`${elementName}-testimonial-job-${index}`}>
+        <div className={`${baseClass}__testimonial-info`}>
+          <h4 className={`${baseClass}__testimonial-name`} data-lfr-editable-id={`experience-name-${index}`} data-lfr-editable-type="text">
+            {user.name}
+          </h4>
+          <Paragraph className={`${baseClass}__testimonial-job`} id={`${elementName}-testimonial-job-${index}`} isEditable={false}>
             {user.job}
           </Paragraph>
         </div>
-        <a href={user.linkedin} className="testimonial-linkedin">
+        <a
+          href={user.linkedin}
+          className={`${baseClass}__testimonial-linkedin`}
+          data-lfr-editable-id={`experience-linkedin-${index}`}
+          data-lfr-editable-type="link">
           <i className="ph ph-linkedin-logo"></i>
         </a>
       </div>
@@ -108,10 +101,16 @@ const Experiencia = () => {
   )
 
   // Componente para renderizar video
-  const VideoCard = ({ src, link }) => (
-    <a href={link} className="card-link-video">
-      <div className="video-card-7">
-        <video src={src} className="experience-carousel__video" autoPlay={true} loop={true} muted={true} playsInline />
+  const VideoCard = ({ videoId, link, index }) => (
+    <a
+      href={link}
+      className={`${baseClass}__card-link`}
+      data-lfr-editable-id={`experience-video-link-${index}`}
+      data-lfr-editable-type="link">
+      <div className={`${baseClass}__video-card`}>
+        <div className={`${baseClass}__video-container`} data-video-id={videoId}>
+          {/* El video se cargará via JavaScript */}
+        </div>
       </div>
     </a>
   )
@@ -120,42 +119,48 @@ const Experiencia = () => {
   const renderCard = (item, index) => {
     switch (item.type) {
       case 'image':
-        return <ImageCard key={index} {...item} />
+        return <ImageCard key={index} {...item} index={index} />
       case 'testimonial':
         return <TestimonialCard key={index} {...item} index={index} />
       case 'video':
-        return <VideoCard key={index} {...item} />
+        return <VideoCard key={index} {...item} index={index} />
       default:
         return null
     }
   }
 
   return (
-    <section id="section-seven">
-      <Container className="container experience-carousel">
-        <Title hierarchy="h2" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-title`}>
+    <section className={`${baseClass}_container`}>
+      <Container id={elementName} className={baseClass}>
+        <Title hierarchy="h2" className={`${baseClass}__title`} id={`${elementName}-title`} size="2xl" weight="semibold" align="center">
           Vive la Experiencia Javeriana
         </Title>
-        <Paragraph className={`${baseClass}__description`} id={`${elementName}-description`}>
+
+        <Paragraph className={`${baseClass}__description`} id={`${elementName}-description`} align="center">
           Descubre historias inspiradoras, momentos únicos y experiencias de nuestros estudiantes a través de sus palabras, imágenes y
           videos.
         </Paragraph>
-        <div className="container swiper">
-          <div className="card-wrapper experience-swiper">
-            <ul className="card-list-7 swiper-wrapper">
+
+        <div className={`${baseClass}__carousel swiper`}>
+          <div className={`${baseClass}__wrapper experience-swiper`}>
+            <div className={`${baseClass}__slides swiper-wrapper`} role="list">
               {carouselData.map((item, index) => (
-                <li key={index} className="card-item-7 swiper-slide">
+                <div key={index} className={`${baseClass}__slide swiper-slide`} role="listitem">
                   {renderCard(item, index)}
-                </li>
+                </div>
               ))}
-            </ul>
+            </div>
+
+            {/* Paginación */}
+            <div className={`swiper-pagination ${baseClass}__pagination`} role="tablist" aria-label="Control de páginas del carrusel"></div>
+
             {/* Botones de navegación */}
-            <div className="swiper-slide-button experience-prev">
-              <i className="ph ph-arrow-circle-left"></i>
-            </div>
-            <div className="swiper-slide-button experience-next">
-              <i className="ph ph-arrow-circle-right"></i>
-            </div>
+            <button className={`swiper-slide-button ${baseClass}__prev`} aria-label="Ir al slide anterior" type="button">
+              <i className="ph ph-arrow-circle-left" aria-hidden="true"></i>
+            </button>
+            <button className={`swiper-slide-button ${baseClass}__next`} aria-label="Ir al siguiente slide" type="button">
+              <i className="ph ph-arrow-circle-right" aria-hidden="true"></i>
+            </button>
           </div>
         </div>
       </Container>
