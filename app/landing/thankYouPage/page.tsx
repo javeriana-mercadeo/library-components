dc71e488481feac8fd9f71f9a545f42adf66a838
Jose Moreno <jgmorenor277@gmail.com>
2025-06-07 17:52:51 -0500

    Creación de template para la sección en lfr

--- app/landing/thankYouPage/page.tsx
+++ app/landing/thankYouPage/page.tsx
@@ -11,17 +11,15 @@ import thanksImg from './assets/53593052f5a8a0826aa368dad23a6c16182a1c37.jpg'
 import info from './info.json'
 import './styles.scss'
 
-
 export default function ThankYouPage() {
+  const elementName = info.id || 'thank-you-page'
+  const baseClass = 'thanks'
 
-const elementName = info.id || 'thank-you-page'
-const baseClass = 'thanks'
-
-const renderInfoCard = info;
+  const renderInfoCard = info
 
   return (
     <>
-    <Splash />
+      <Splash />
       <section>
         <Container className={`${baseClass}__main`}>
           <div className={`${baseClass}__pic`}>
@@ -32,7 +30,7 @@ const renderInfoCard = info;
               className={`${baseClass}__image`}
             />
           </div>
-          <div className={`${baseClass}__content`} >
+          <div className={`${baseClass}__content`}>
             <Btn
               href="#"
               target="_blank"
@@ -44,72 +42,76 @@ const renderInfoCard = info;
               Volver al programa
             </Btn>
             <Title hierarchy="h1" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}`}>
-              ¡Gracias por tu interés en ser parte de la Universidad Javeriana! 
+              ¡Gracias por tu interés en ser parte de la Universidad Javeriana!
             </Title>
             <Paragraph id={`${elementName}-enrollment-note`} className={`${baseClass}_enrollment-note`} size="sm">
-              Tu solicitud ha sido recibida, y muy pronto uno de nuestros asesores se pondrá en contacto contigo para brindarte toda la información que necesitas.
+              Tu solicitud ha sido recibida, y muy pronto uno de nuestros asesores se pondrá en contacto contigo para brindarte toda la
+              información que necesitas.
             </Paragraph>
             <div className={`${baseClass}__card-info-main`}>
               <div className={`${baseClass}__card-info`}>
-              <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
-                Explora nuestro tour virtual y conoce la universidad.
-              </Title>
-              <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
-              Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias inolvidables.
-              </Paragraph>
-              <Btn
-                href="#"
-                target="_blank"
-                variant="link"
-                size="md"
-                className={`${baseClass}_card-link`}
-                id={`$elementName`}
-                isEditable={false}>
-                Ver Perfil
-              </Btn>
-            </div>
-            <div className={`${baseClass}__card-info`}>
-              <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
-                Explora nuestro tour virtual y conoce la universidad.
-              </Title>
-              <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
-              Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias inolvidables.
-              </Paragraph>
-              <Btn
-                href="#"
-                target="_blank"
-                variant="link"
-                size="md"
-                className={`${baseClass}_card-link`}
-                id={`$elementName`}
-                isEditable={false}>
-                Ver Perfil
-              </Btn>
-            </div>
-            <div className={`${baseClass}__card-info`}>
-              <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
-                Explora nuestro tour virtual y conoce la universidad.
-              </Title>
-              <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
-              Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias inolvidables.
-              </Paragraph>
-              <Btn
-                href="#"
-                target="_blank"
-                variant="link"
-                size="md"
-                className={`${baseClass}_card-link`}
-                id={`$elementName`}
-                isEditable={false}>
-                Ver Perfil
-              </Btn>
-            </div>
-            </div>
-            <div>
-                <Btn id="registration-btn" isEditable={false}>
-                  ¡Inscríbete Ahora!
+                <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+                  Explora nuestro tour virtual y conoce la universidad.
+                </Title>
+                <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
+                  Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias
+                  inolvidables.
+                </Paragraph>
+                <Btn
+                  href="#"
+                  target="_blank"
+                  variant="link"
+                  size="md"
+                  className={`${baseClass}_card-link`}
+                  id={`$elementName`}
+                  isEditable={false}>
+                  Ver Perfil
                 </Btn>
               </div>
+              <div className={`${baseClass}__card-info`}>
+                <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+                  Explora nuestro tour virtual y conoce la universidad.
+                </Title>
+                <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
+                  Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias
+                  inolvidables.
+                </Paragraph>
+                <Btn
+                  href="#"
+                  target="_blank"
+                  variant="link"
+                  size="md"
+                  className={`${baseClass}_card-link`}
+                  id={`$elementName`}
+                  isEditable={false}>
+                  Ver Perfil
+                </Btn>
+              </div>
+              <div className={`${baseClass}__card-info`}>
+                <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+                  Explora nuestro tour virtual y conoce la universidad.
+                </Title>
+                <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`} size="sm">
+                  Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias
+                  inolvidables.
+                </Paragraph>
+                <Btn
+                  href="#"
+                  target="_blank"
+                  variant="link"
+                  size="md"
+                  className={`${baseClass}_card-link`}
+                  id={`$elementName`}
+                  isEditable={false}>
+                  Ver Perfil
+                </Btn>
+              </div>
+            </div>
+            <div>
+              <Btn id="registration-btn" isEditable={false}>
+                ¡Inscríbete Ahora!
+              </Btn>
+            </div>
           </div>
         </Container>
       </section>
