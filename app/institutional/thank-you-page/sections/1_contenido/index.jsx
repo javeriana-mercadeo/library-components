93bc33fb447a4f9febb7113a1842601582af8541
Jose <jgmorenor277@gmail.com>
2025-06-12 16:46:32 -0500

    Ajustados archivos para lectura en Lfr

--- app/institutional/thank-you-page/sections/1_contenido/index.jsx
+++ app/institutional/thank-you-page/sections/1_contenido/index.jsx
@@ -16,7 +16,7 @@ export default function ThankYouPage() {
   const renderInfoCard = info
 
   return (
-    <Container className={`${baseClass}__main`}>
+    <Container id={elementName} className={`${baseClass}__main`}>
       <div className={`${baseClass}__pic`}>
         <Image
           id={`${elementName}-image`}
@@ -26,26 +26,36 @@ export default function ThankYouPage() {
         />
       </div>
       <div className={`${baseClass}__content`}>
-        <Btn href="#" target="_blank" variant="link" className={`${baseClass}__card-link`} id={`$elementName`} isEditable={false}>
+        <Btn
+          href="#"
+          target="_blank"
+          variant="link"
+          className={`${baseClass}__card-link`}
+          id={`${elementName}-back-to-program`}
+          isEditable={false}>
           <i className="ph ph-arrow-bend-up-left"></i>
           Volver al programa
         </Btn>
-        <Title hierarchy="h1" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}`}>
+
+        <Title hierarchy="h1" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-main-title`}>
           ¡Gracias por tu interés en ser parte de la Universidad Javeriana!
         </Title>
+
         <Paragraph id={`${elementName}-enrollment-note`} className={`${baseClass}_enrollment-note`}>
           Tu solicitud ha sido recibida, y <strong>muy pronto uno de nuestros asesores se pondrá en contacto contigo</strong> para brindarte
           toda la información que necesitas.
         </Paragraph>
+
         <div className={`${baseClass}__card-info-main`}>
+          {/* CARD 1 - Tour Virtual */}
           <div className={`${baseClass}__card-info`}>
             <div className={`${baseClass}__card-info-icon`}>
               <i className="ph ph-panorama"></i>
             </div>
-            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-subtitle-explore`}>
               Explora nuestro tour virtual y conoce la universidad.
             </Title>
-            <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`}>
+            <Paragraph id={`${elementName}-tour-description`} className={`${baseClass}_enrollment-note`}>
               Recorre cada espacio de nuestra universidad sin moverte de casa. Conoce los lugares donde vivirás experiencias inolvidables.
             </Paragraph>
             <Btn
@@ -54,20 +64,22 @@ export default function ThankYouPage() {
               variant="link"
               size="md"
               className={`${baseClass}_card-link`}
-              id={`$elementName`}
+              id={`${elementName}-tour-btn`}
               isEditable={false}>
               Ver tour
               <i className="ph ph-arrow-up-right"></i>
             </Btn>
           </div>
+
+          {/* CARD 2 - Eventos */}
           <div className={`${baseClass}__card-info`}>
             <div className={`${baseClass}__card-info-icon`}>
               <i className="ph ph-calendar-dots"></i>
             </div>
-            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-subtitle-events`}>
               Eventos, actividades y apoyo para tu futuro académico.
             </Title>
-            <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`}>
+            <Paragraph id={`${elementName}-events-description`} className={`${baseClass}_enrollment-note`}>
               Charlas, talleres y ferias académicas diseñadas para ti. Descubre becas, financiación y mucho más.
             </Paragraph>
             <Btn
@@ -76,20 +88,22 @@ export default function ThankYouPage() {
               variant="link"
               size="md"
               className={`${baseClass}_card-link`}
-              id={`$elementName`}
+              id={`${elementName}-events-btn`}
               isEditable={false}>
               Conoce más
               <i className="ph ph-arrow-up-right"></i>
             </Btn>
           </div>
+
+          {/* CARD 3 - Contacto */}
           <div className={`${baseClass}__card-info`}>
             <div className={`${baseClass}__card-info-icon`}>
               <i className="ph ph-telegram-logo"></i>
             </div>
-            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-`}>
+            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-subtitle-contact`}>
               Estamos aquí para responder todas tus preguntas.
             </Title>
-            <Paragraph id={`${elementName}-tour-info`} className={`${baseClass}_enrollment-note`}>
+            <Paragraph id={`${elementName}-contact-description`} className={`${baseClass}_enrollment-note`}>
               Contáctanos por correo o teléfono para recibir información sobre becas, financiación y admisiones.
             </Paragraph>
             <Btn
@@ -98,15 +112,40 @@ export default function ThankYouPage() {
               variant="link"
               size="md"
               className={`${baseClass}_card-link`}
-              id={`$elementName`}
+              id={`${elementName}-contact-btn`}
               isEditable={false}>
               Escríbenos
               <i className="ph ph-arrow-up-right"></i>
             </Btn>
           </div>
+
+          {/* CARD 4 - Centro de Ayuda */}
+          <div className={`${baseClass}__card-info`}>
+            <div className={`${baseClass}__card-info-icon`}>
+              <i className="ph ph-storefront"></i>
+            </div>
+            <Title hierarchy="h3" data-puj-name="true" className={`${baseClass}_title`} id={`${elementName}-subtitle-help`}>
+              Todo lo que necesitas saber, en un solo lugar.
+            </Title>
+            <Paragraph id={`${elementName}-help-description`} className={`${baseClass}_enrollment-note`}>
+              Desde procesos de inscripción hasta opciones de financiación. Explora nuestro Centro de Ayuda.
+            </Paragraph>
+            <Btn
+              href="#"
+              target="_blank"
+              variant="link"
+              size="md"
+              className={`${baseClass}_card-link`}
+              id={`${elementName}-help-center-btn`}
+              isEditable={false}>
+              Ir al centro de ayuda
+              <i className="ph ph-arrow-up-right"></i>
+            </Btn>
+          </div>
         </div>
+
         <div className={`${baseClass}__btn-main`}>
-          <Btn id="registration-btn" isEditable={false}>
+          <Btn id={`${elementName}-view-programs-btn`} isEditable={false}>
             Ver más programas
           </Btn>
         </div>
