af8c1af2c978dbda9745415e3da11469857a9f96
Jose Moreno <jgmorenor277@gmail.com>
2025-06-15 19:27:40 -0500

    Restablecida la sección de materias por semestre en pregrado

--- build/compiled-scripts.js
+++ build/compiled-scripts.js
@@ -1,5 +1,5 @@
 // ===== LIBRERÍAS EXTERNAS =====
-// Compilado automáticamente el: 2025-06-15T22:02:41.364Z
+// Compilado automáticamente el: 2025-06-16T00:23:26.251Z
 
 var head = document.getElementsByTagName("head")[0];
 
