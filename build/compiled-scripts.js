778f08f055021ea4e421a20aa64af9c75380ef10
Jose <jgmorenor277@gmail.com>
2025-06-19 16:21:50 -0500

    Resuelto problema de sincronización entre indicador de slide y slide principal vs rep de video

--- build/compiled-scripts.js
+++ build/compiled-scripts.js
@@ -1,5 +1,5 @@
 // ===== LIBRERÍAS EXTERNAS =====
-// Compilado automáticamente el: 2025-06-17T19:45:55.339Z
+// Compilado automáticamente el: 2025-06-19T20:37:19.641Z
 
 var head = document.getElementsByTagName("head")[0];
 
