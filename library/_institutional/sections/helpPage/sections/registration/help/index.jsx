c20f9738b124d709cd1e1fc6ea30b2f07c8f5266
krlaNicol <karla.gonzalez@genstudents.org>
2025-07-11 15:47:42 -0500

    add: components a help

--- library/_institutional/sections/helpPage/sections/registration/help/index.jsx
+++ library/_institutional/sections/helpPage/sections/registration/help/index.jsx
@@ -1,39 +1,46 @@
 import React from 'react'
 import './styles.scss'
-import Title From ""
+import Container from '@library/components/container'
+import Title from '@library/components/contain/title'
+import Image from '@library/components/contain/image'
+import Paragraph from '@library/components/contain/paragraph'
 
 const Index = () => {
   return (
     <section className="help-section">
-      <div className="help-header">
-       <Title className="title-help"> ¿Necesitas ayuda?</Title>
-        <a href="/institutional/helpPage/pgInscription">
-          Ver más <i className="ph ph-arrow-right"></i>
-        </a>
-      </div>
-
-      <div className="help-links">
-        <div className="help-item">
-          <a href="/institutional/helpPage/questionFrequently">
-            Proceso de Inscripción <i className="ph ph-arrow-up-right"></i>
+      <Container>
+        <div className="help-header">
+          <Title className="title-help"> ¿Necesitas ayuda?</Title>
+          <a href="/institutional/helpPage/pgInscription">
+            Ver más <i className="ph ph-arrow-right"></i>
           </a>
-          <p>Todo lo que necesitas saber para inscribirte en la Universidad Javeriana.</p>
         </div>
 
-        <div className="help-item">
-          <a href="/institutional/helpPage/financing/financingQuestionFrequently">
-            Becas y Financiación <i className="ph ph-arrow-up-right"></i>
-          </a>
-          <p>Opciones de apoyo económico para facilitar tu ingreso a la universidad.</p>
-        </div>
+        <div className="help-links">
+          <div className="help-item">
+            <a href="/institutional/helpPage/questionFrequently">
+              Proceso de Inscripción <i className="ph ph-arrow-up-right"></i>
+            </a>
+            <Paragraph>
+              Todo lo que necesitas saber para inscribirte en la Universidad Javeriana.
+            </Paragraph>
+          </div>
 
-        <div className="help-item">
-          <a href="/institutional/helpPage/internationalization/internationalizationQuestionFrequently">
-            Internacionalización y Movilidad <i className="ph ph-arrow-up-right"></i>
-          </a>
-          <p>Oportunidades de estudios en el extranjero y experiencias internacionales.</p>
+          <div className="help-item">
+            <a href="/institutional/helpPage/financing/financingQuestionFrequently">
+              Becas y Financiación <i className="ph ph-arrow-up-right"></i>
+            </a>
+            <Paragraph>Opciones de apoyo económico para facilitar tu ingreso a la universidad. </Paragraph>
+          </div>
+
+          <div className="help-item">
+            <a href="/institutional/helpPage/internationalization/internationalizationQuestionFrequently">
+              Internacionalización y Movilidad <i className="ph ph-arrow-up-right"></i>
+            </a>
+             <Paragraph>Oportunidades de estudios en el extranjero y experiencias internacionales. </Paragraph>
+          </div>
         </div>
-      </div>
+      </Container>
     </section>
   )
 }
