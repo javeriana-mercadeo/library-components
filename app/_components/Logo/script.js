84a67537539f248639f1022f854c4efa2ed62871
Eli RoCas <elir_94@hotmail.com>
2025-07-13 19:42:21 -0500

    add: dynamic logo (by faculties and by years)

---
+++ app/_components/Logo/script.js
@@ -0,0 +1,17 @@
+const fragmentElement = document
+
+// document.addEventListener("DOMContentLoaded", () => {
+const logo = fragmentElement.querySelector('#variation-logo-fragment')
+const logoImg = fragmentElement.querySelector('.logo-wrapper')
+logoImg.style.width=`${configuration.width || 14}%`
+logoImg.style.height=`${configuration.height || 12}%`
+
+const targetContainer = fragmentElement.querySelector('.program-data_faculty-container')
+// const targetContainer = fragmentElement.querySelector('.program-data_title-program')
+
+if (logo && targetContainer) {
+  targetContainer.style.position = 'relative'
+  targetContainer.appendChild(logo)
+//   targetContainer.insertBefore(logo, targetContainer.firstChild)
+}
+// });
