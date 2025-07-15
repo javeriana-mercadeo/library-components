9b3db60d9036dbd9d0c10bab6fb79ee9de106682
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2025-07-15 09:02:57 -0500

    fix: se ajustan las rutas de form-modules

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* <!-- Flowbite CSS --> */}
         <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-        <link rel="stylesheet" href="https://intranet.javeriana.edu.co/documents/d/info-prg/form-modules-style" />
+        <link rel="stylesheet" href="https://www.javeriana.edu.co/recursosdb/d/info-prg/form-modules-style" />
         {/* Utilidades globales - DEBE CARGARSE PRIMERO */}
         <title>Librería de componentes</title>
       </head>
@@ -72,7 +72,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <script src="https://www.javeriana.edu.co/planestudio/pages/libraries/simple_datatables/simple-datatables.js"></script>
 
         {/* Form-modules */}
-        <script src="https://intranet.javeriana.edu.co/documents/d/info-prg/form-modules-script"></script>
+        <script src="https://www.javeriana.edu.co/recursosdb/d/info-prg/form-modules-script"></script>
       </body>
     </html>
   )
