c607b4a89ee4f91e40170f2cfe07c1b683b6daf5
J. Alexander Gavilán M. <allexander.gm@gmail.com>
2025-07-16 17:17:11 -0500

    add: se agrega el formulario

--- app/landing/pregrado/_sections/0_encabezado/components/modalFrom.js
+++ app/landing/pregrado/_sections/0_encabezado/components/modalFrom.js
@@ -2,29 +2,32 @@
 // FORM MANAGER - Integración con FormModules
 // ===========================================
 
-// Configuración del formulario
-const configModalForm = {
-  eventName: 'Open Day Pregrado Javeriana 2024',
-  eventDate: '2024-07-15',
-  campaign: 'PREGRADO_MODAL_2024',
-  article: 'landing_pregrado',
-  source: 'modal_landing',
-  medium: 'organico',
-  typeAttendee: ['Aspirante'],
-  programs: ['PSICO'],
-  sandboxMode: true,
-  debugMode: true,
-  devMode: false,
-  debugEmail: 'gavilanm-j@javeriana.edu.co'
-}
-
 // Instanciar FormModules
 const ModalForm = {
   formModulesInstance: null,
 
   async init() {
-    this.formModulesInstance = new window.FormModules('formProgram', configModalForm)
-    await this.formModulesInstance.initialize()
+    document.addEventListener('data_load-program', async event => {
+      const { programa, codPrograma } = event.detail.dataProgram
+      console.log(`Iniciando FormModules para el programa: ${programa} con código: ${codPrograma}`)
+
+      this.formModulesInstance = new window.FormModules('formProgram', {
+        eventName: `Landing Page: ${programa}`,
+        eventDate: '2024-07-15',
+        campaign: 'PREGRADO_MODAL_2024',
+        article: 'landing_pregrado',
+        source: 'modal_landing',
+        medium: 'organico',
+        typeAttendee: ['Aspirante'],
+        programs: [codPrograma],
+        sandboxMode: true,
+        debugMode: true,
+        devMode: false,
+        debugEmail: 'gavilanm-j@javeriana.edu.co'
+      })
+
+      await this.formModulesInstance.initialize()
+    })
   },
 
   cleanup() {
