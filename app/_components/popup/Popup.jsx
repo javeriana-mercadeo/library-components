0e2284fb2a4214775899bb5dc64300868da6efe2
Eli RoCas <elir_94@hotmail.com>
2025-07-16 07:31:20 -0500

    update: script para visualización del boton.

--- app/_components/popup/Popup.jsx
+++ app/_components/popup/Popup.jsx
@@ -2,11 +2,12 @@ import { useEffect } from 'react'
 import PopupContainer from '@/app/_components/popup/components/Popup-container.jsx'
 import Content from '@/app/_components/popup/components/Content.jsx'
 import TriggerBtn from '@/app/_components/popup/components/Trigger-btn'
+import script from './script.js'
 // import './popup.scss'
 
 const Popup = () => {
   useEffect(() => {
-    import('./script.js')
+    script()
   }, [])
   return (
     <>
--- app/_components/popup/script.js
+++ app/_components/popup/script.js
@@ -2,43 +2,55 @@
 // isEditMode is a variable used in liferay that avoids running scripts in edit mode and allows editing //
 // =====================================================================================================//
 
-const fragment = document
-const configuration = {
-  autoOpen : false,
-}
-// const fragment = fragmentElement;
+export default () => {
+  const fragment = document
+  const configuration = {
+    autoOpen: true,
+    showOpenButton: false
+  }
 
-// if (!isEditMode) {
-setupPopupEvent('[data-popup]', 'click', element => {
-  const popup = document.querySelector(element.target.dataset.popup)
-  popup.style.display = 'flex'
-  console.log('Popup opened:', popup)
-})
+  // const fragment = fragmentElement;
 
-setupPopupEvent('.popup-close', 'click', element => {
-  const popup = element.target.closest('.popup-overlay')
-  popup.style.display = 'none'
-})
-
-setupPopupEvent('.popup-content', 'click', element => {
-  element.stopPropagation()
-})
-
-setupPopupEvent('.popup-overlay', 'click', element => {
-  element.target.style.display = 'none'
-})
-
- const autoPopup = fragment.querySelector('.popup-overlay')
-if (configuration.autoOpen) {
-  autoPopup.style.display = 'flex'
-}
-
-function setupPopupEvent(selector, event, callback) {
-  fragment.querySelectorAll(selector).forEach(el => {
-    el.addEventListener(event, callback)
+  // if (!isEditMode) {
+  setupPopupEvent('[data-popup]', 'click', element => {
+    const popup = document.querySelector(element.target.dataset.popup)
+    popup.style.display = 'flex'
+    console.log('Popup opened:', popup)
   })
+
+  setupPopupEvent('.popup-close', 'click', element => {
+    const popup = element.target.closest('.popup-overlay')
+    popup.style.display = 'none'
+  })
+
+  setupPopupEvent('.popup-content', 'click', element => {
+    element.stopPropagation()
+  })
+
+  setupPopupEvent('.popup-overlay', 'click', element => {
+    element.target.style.display = 'none'
+  })
+
+  setupPopupEvent('.content-btn', 'click', element => {
+    const popup = element.target.closest('.popup-overlay')
+    popup.style.display = 'none'
+  })
+
+  const autoPopup = fragment.querySelector('.popup-overlay')
+  if (configuration.autoOpen) {
+    autoPopup.style.display = 'flex'
+  }
+
+  const showOpenBtn = fragment.querySelector('.popup-open__btn')
+  if (configuration.showOpenButton) {
+    showOpenBtn.style.display = 'none'
+  }
+
+  function setupPopupEvent(selector, event, callback) {
+    fragment.querySelectorAll(selector).forEach(el => {
+      el.addEventListener(event, callback)
+    })
+  }
+
+  // }
 }
-
-// }
-
-
