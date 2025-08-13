32d97cb89d3e4748520411946c46e33711c9cf20
krlaNicol <karla.gonzalez@genstudents.org>
2025-08-12 19:31:33 -0500

    estructura de la seccion video doctorado

--- app/landing/doctorado/page.tsx
+++ app/landing/doctorado/page.tsx
@@ -1,3 +1,17 @@
-export default function Doctorado() {
-  return <div></div>
+'use client'
+import ViewComponent from '@/app/_components/viewComponent/viewComponent'
+
+import VideoDoctorado from './sections/videoDoctorado'
+
+export default function SeccionesOpcionales() {
+  const basePath = '/landing/doctorado'
+  return (
+    <>
+       <VideoDoctorado />
+       <ViewComponent path={`${basePath}/sections/videoDoctorado`}>
+        <VideoDoctorado />
+      </ViewComponent> 
+
+    </>
+  )
 }
