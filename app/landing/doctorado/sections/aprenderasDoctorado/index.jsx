8833f6db7deef60ef26ec594934c86f15dab980f
krlaNicol <karla.gonzalez@genstudents.org>
2025-08-27 10:12:39 -0500

    modificaciones en la seccion de aprender doctorado

--- app/landing/doctorado/sections/aprenderasDoctorado/index.jsx
+++ app/landing/doctorado/sections/aprenderasDoctorado/index.jsx
@@ -4,7 +4,7 @@ import './styles.scss'
 const Aprender = () => {
   return (
     <section id="section-six">
-      <div className="color-container">
+      <div className="color-containerA">
         <div className="container expert-carousel" id="docentes">
           <h2
             className="title title-neutral title-2xl title-center title-bold expert-carousel__title"
@@ -13,10 +13,10 @@ const Aprender = () => {
             ¿Qué aprenderás en el Doctorado en Ciencias Sociales y Humanas?
           </h2>
 
-          <div className="contentGeneralD">
+          <div className="contentGeneralA">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmv36c8jSv-3h7C7qV5RRZMrlUGi_0etVxLQ&s"
-              className="imgDoctorado"
+              className="imgDoctoradoA"
               alt="Foto del docente"
               data-lfr-editable-id="image-1"
               data-lfr-editable-type="image"
@@ -24,6 +24,14 @@ const Aprender = () => {
             <div className="doctoradoGrid">
 
               <p className="descriptionDoctorado" data-lfr-editable-id="docente-description-1" data-lfr-editable-type="rich-text">
+                El Doctorado en Ciencias Sociales y Humanas, contribuye a la apropiación, aplicación, generación y transferencia de
+                conocimiento pertinente para la comprensión y explicación de los complejos problemas sociales y humanos en un contexto
+                globalizado, a través de una estrategia integradora de conocimientos. Forma investigadoras e investigadores de alto nivel
+                capaces de contribuir al desarrollo de las ciencias sociales y humanas, y al fortalecimiento de la sociedad civil.<br></br>
+                El Doctorado en Ciencias Sociales y Humanas, contribuye a la apropiación, aplicación, generación y transferencia de
+                conocimiento pertinente para la comprensión y explicación de los complejos problemas sociales y humanos en un contexto
+                globalizado, a través de una estrategia integradora de conocimientos. Forma investigadoras e investigadores de alto nivel
+                capaces de contribuir al desarrollo de las ciencias sociales y humanas, y al fortalecimiento de la sociedad civil.
                 El Doctorado en Ciencias Sociales y Humanas, contribuye a la apropiación, aplicación, generación y transferencia de
                 conocimiento pertinente para la comprensión y explicación de los complejos problemas sociales y humanos en un contexto
                 globalizado, a través de una estrategia integradora de conocimientos. Forma investigadoras e investigadores de alto nivel
