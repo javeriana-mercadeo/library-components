5ba576adb38d584e1cd28bc0a51957c2c09a4848
Jose Moreno <jgmorenor277@gmail.com>
2025-08-27 06:44:11 -0500

    Completados todos los ajustes

--- app/landing/pregrado/_sections/_becas/index.jsx
+++ app/landing/pregrado/_sections/_becas/index.jsx
@@ -16,24 +16,29 @@ const Becas = () => {
       id: 'becas',
       label: 'Nuestras Becas',
       icon: 'ph-graduation-cap',
-      image: 'https://www.javeriana.edu.co/recursosdb/20125/12256677/apoyo-financiero-y-descuentos.JPG/25b2a8f4-71f3-da2e-2728-b8af1111a992',
+      image:
+        'https://www.javeriana.edu.co/recursosdb/20125/12256677/apoyo-financiero-y-descuentos.JPG/25b2a8f4-71f3-da2e-2728-b8af1111a992',
       imageAlt: 'Apoyo financiero y descuentos - Becas Universidad Javeriana',
       title: 'Becas disponibles',
       content: (
         <>
+          <p>Contamos con un área de apoyo financiero que ofrece diferentes becas para que inicies tus estudios de pregrado.</p>
           <p>
-            Contamos con un área de apoyo financiero que ofrece diferentes becas para que inicies tus estudios de pregrado.
-          </p>
-          <p>
-            La Universidad ofrece opciones de becas* para aspirantes admitidos a pregrado con excelente desempeño académico y/o con 
+            La Universidad ofrece opciones de becas* para aspirantes admitidos a pregrado con excelente desempeño académico y/o con
             dificultades económicas. Explora los requisitos para aplicar a:
           </p>
           <ul>
-            <li><strong>Beca Ingresa a la Javeriana</strong></li>
-            <li><strong>Beca Bachiller Destacado</strong></li>
-            <li><strong>Beca Bachiller Destacado ACODESI</strong></li>
+            <li>
+              <strong>Beca Ingresa a la Javeriana</strong>
+            </li>
+            <li>
+              <strong>Beca Bachiller Destacado</strong>
+            </li>
+            <li>
+              <strong>Beca Bachiller Destacado ACODESI</strong>
+            </li>
           </ul>
-          <p className="terms-note">*Aplican términos y condiciones.</p>
+          <p className='terms-note'>*Aplican términos y condiciones.</p>
         </>
       ),
       buttonText: 'Más información',
@@ -43,16 +48,23 @@ const Becas = () => {
       id: 'financiacion',
       label: 'Programa de Financiación',
       icon: 'ph-credit-card',
-      image: 'https://www.javerianacali.edu.co/sites/default/files/styles/image_1100x700/public/2020-02/programas-academicos-1100x700.jpg.webp?itok=MglNGFBd',
+      image:
+        'https://www.javerianacali.edu.co/sites/default/files/styles/image_1100x700/public/2020-02/programas-academicos-1100x700.jpg.webp?itok=MglNGFBd',
       imageAlt: 'Programas académicos - Opciones de financiación Universidad Javeriana',
       title: 'Opciones de financiación',
       content: (
         <>
           <p>También contamos con opciones de financiación.</p>
           <ul>
-            <li><strong>A corto plazo</strong> directamente con la Universidad, tasa de interés preferencial.</li>
-            <li><strong>A mediano y largo plazo</strong> con entidades aliadas que ofrecen bajas tasas de interés.</li>
-            <li>Conoce, también, nuestros <strong>descuentos y convenios</strong> con entidades públicas y privadas.</li>
+            <li>
+              <strong>A corto plazo</strong> directamente con la Universidad, tasa de interés preferencial.
+            </li>
+            <li>
+              <strong>A mediano y largo plazo</strong> con entidades aliadas que ofrecen bajas tasas de interés.
+            </li>
+            <li>
+              Conoce también, nuestros <strong>descuentos y convenios</strong> con entidades públicas y privadas.
+            </li>
           </ul>
         </>
       ),
@@ -104,11 +116,7 @@ const Becas = () => {
                 <div className={`${baseClass}__content-wrapper`}>
                   <div className={`${baseClass}__media-container`}>
                     {tab.image ? (
-                      <Image 
-                        src={tab.image} 
-                        alt={tab.imageAlt} 
-                        className={`${baseClass}__content-image`}
-                      />
+                      <Image src={tab.image} alt={tab.imageAlt} className={`${baseClass}__content-image`} />
                     ) : (
                       <Icon icon={tab.icon} size='lg' className={`${baseClass}__content-icon`} />
                     )}
@@ -117,16 +125,13 @@ const Becas = () => {
                     <Caption className={`${baseClass}__content-title`} size='xl' weight='semibold' color='neutral'>
                       {tab.title}
                     </Caption>
-                    <div className={`${baseClass}__content-text`}>
-                      {tab.content}
-                    </div>
+                    <div className={`${baseClass}__content-text`}>{tab.content}</div>
                     <Button
                       className={`${baseClass}__cta-button`}
                       variant='solid'
                       color='primary'
                       size='md'
-                      href={tab.buttonLink}
-                      startIcon={<Icon icon='ph-arrow-right' />}>
+                      href={tab.buttonLink}>
                       {tab.buttonText}
                     </Button>
                   </div>
