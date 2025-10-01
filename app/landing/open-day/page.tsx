925a453abc03a0bb7b3d5d491b0ff8283f107645
krlaNicol <krlanicol@gmail.com>
2025-10-01 13:47:13 -0500

    seccion de herramientas corregida respecto a lo que trae liferay

---
+++ app/landing/open-day/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import ViewComponent from '@/app/_components/viewComponent/viewComponent'
+
+import Temporizador from './sections/temporizador'
+import DocentesDoctorado from './sections/docentesDoctorado'
+import AprenderasDoctorado from './sections/aprenderasDoctorado'
+
+
+
+export default function Doctorados() {
+  const basePath = '/landing/open-day'
+  return (
+    <>
+
+      {/* <VideoDoctorado /> */}
+      <ViewComponent path={`${basePath}/sections/temporizador`}>
+        <Temporizador />
+      </ViewComponent>
+            <ViewComponent path={`${basePath}/sections/docentesDoctorado`}>
+        <DocentesDoctorado />
+      </ViewComponent>
+          
+            <ViewComponent path={`${basePath}/sections/aprenderasDoctorado`}>
+        <AprenderasDoctorado />
+      </ViewComponent>
+
+
+    </>
+  )
+}
