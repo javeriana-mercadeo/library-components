3551aa896b1615bbf1f5bd7276e322d031b11103
Jose Moreno <jgmorenor277@gmail.com>
2025-10-02 06:47:49 -0500

    Creados los primeros elementos del header

--- app/institutional/expoJaverianaPrg/_sections/1_header/index.jsx
+++ app/institutional/expoJaverianaPrg/_sections/1_header/index.jsx
@@ -17,7 +17,21 @@ const HeaderExpo = () => {
 
   return (
     <div className={baseClass} id={elementName}>
-      {/* Contenido del header aqui */}
+      <div className={`${baseClass}__container`}>
+        {/* Columna Izquierda - Logo */}
+        <div className={`${baseClass}__left`}>
+          <img
+            src="https://www.javeriana.edu.co/recursosdb/d/info-prg/logo-expo-javeriana-2026"
+            alt="Logo Expo Javeriana 2026"
+            className={`${baseClass}__logo`}
+          />
+        </div>
+
+        {/* Columna Derecha */}
+        <div className={`${baseClass}__right`}>
+          {/* Contenido derecho aqui */}
+        </div>
+      </div>
     </div>
   )
 }
